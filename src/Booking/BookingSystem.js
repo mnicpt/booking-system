043642a4e7229672b88f50f153bf80185758807f
Steve Mask <mnicpt@gmail.com>
2019-07-16 21:01:49 -0500

    linting

--- src/Booking/BookingSystem.js
+++ src/Booking/BookingSystem.js
@@ -1,9 +1,25 @@
+/**
+ * BookingSystem.js
+ *
+ * @return {object}
+ * {
+ *  addRoom,
+ *  removeRoom,
+ *  getRoom,
+ *  bookRoom,
+ *  releaseRoom,
+ *  availableRooms,
+ *  calculateTotalCost
+ * }
+ */
 export default function BookingSystem() {
   const rooms = {};
   let roomCount = 0;
 
   const addRoom = (room) => {
-    if (!room.level || !room.beds) throw new Error('Parameter must be of type Room.');
+    if (!room.level || !room.beds) {
+      throw new Error('Parameter must be of type Room.');
+    }
     rooms[++roomCount] = room;
   };
 
@@ -28,14 +44,27 @@ export default function BookingSystem() {
   };
 
   const availableRooms = () => {
-    return Object.keys(rooms).filter((roomNumber) => getRoom(roomNumber).booked === false);
+    return Object.keys(rooms).filter((roomNumber) =>
+      getRoom(roomNumber).booked === false);
   };
 
   const calculateTotalCost = (booking) => {
-    if (!booking.room || !booking.startDate || !booking.endDate || !booking.pets || booking.accessible !== undefined) throw new Error('Parameter must be a Booking.');
+    if (!booking.room || !booking.startDate ||
+      !booking.endDate || !booking.pets ||
+      booking.accessible !== undefined) {
+      throw new Error('Parameter must be a Booking.');
+    }
     const nights = (booking.endDate - booking.startDate) * 1000 * 60 * 60 * 24;
     return booking.room.cost * nights + (booking.pets > 0 ? 20 : 0);
   };
 
-  return {addRoom, removeRoom, getRoom, bookRoom, releaseRoom, availableRooms, calculateTotalCost};
+  return {
+    addRoom,
+    removeRoom,
+    getRoom,
+    bookRoom,
+    releaseRoom,
+    availableRooms,
+    calculateTotalCost,
+  };
 }
--- src/Room/Room.js
+++ src/Room/Room.js
@@ -1,13 +1,31 @@
+/**
+ * Room.js
+ *
+ * @param  {...any} args {beds, level}
+ * @return {object}
+ * {
+ *  beds,
+ *  level,
+ *  accessible,
+ *  petsAllowed,
+ *  cost,
+ *  booked
+ * }
+ */
 export default function Room(...args) {
-  const {beds, level} = args[0];
+  const { beds, level } = args[0];
   const roomCosts = {
     1: 50,
     2: 75,
     3: 90,
   };
 
-  if (level < 1 || level > 2) throw new Error('Allowed number of levels are 1, or 2.');
-  if (beds < 1 || beds > 3) throw new Error('Allowed number of beds are 1, 2, or 3.');
+  if (level < 1 || level > 2) {
+    throw new Error('Allowed number of levels are 1, or 2.');
+  }
+  if (beds < 1 || beds > 3) {
+    throw new Error('Allowed number of beds are 1, 2, or 3.');
+  }
 
   const accessible = level === 1;
   const petsAllowed = level === 1;
@@ -15,5 +33,5 @@ export default function Room(...args) {
 
   this.booked = false;
 
-  return {beds, level, accessible, petsAllowed, cost, booked: this.booked};
+  return { beds, level, accessible, petsAllowed, cost, booked: this.booked };
 }
